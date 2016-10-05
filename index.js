cce1bc366f2aabac064286f04f281bf5c71d3caa
Bjørge Næss <bjoerge@gmail.com>
2016-10-05 12:52:46 +0200

    Rename function

--- index.js
+++ index.js
@@ -1,4 +1,4 @@
-module.exports = function atAny (array, index) {
+module.exports = function circularAt (array, index) {
   const length = array && array.length
   var idx = Math.abs(length + index % length) % length
   return array[isNaN(idx) ? index : idx]

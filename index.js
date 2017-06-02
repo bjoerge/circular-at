7c7402a3199839e212e8c589c203b8d776ef47f3
Espen Hovlandsdal <espen@hovlandsdal.com>
2017-06-02 13:27:10 +0200

    Replace const with var

--- index.js
+++ index.js
@@ -1,5 +1,5 @@
 module.exports = function circularAt (array, index) {
-  const length = array && array.length
+  var length = array && array.length
   var idx = Math.abs(length + index % length) % length
   return array[isNaN(idx) ? index : idx]
 }
